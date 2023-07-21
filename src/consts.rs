acf50c41b4a74f970addcacb2adffeb6dac26024
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2023-07-21 08:30:47 -0700

    Clarify source of thresholds, rename const for clarity

--- src/consts.rs
+++ src/consts.rs
@@ -1,16 +1,16 @@
 pub mod f64 {
-    // Improved thresholds for `ln_1p_exp` based on
-    // https://github.com/JuliaStats/LogExpFunctions.jl/files/8218470/log1pexp.pdf
-    // which is itself a derivative of
+    // Thresholds for `ln_1p_exp` based on
     // http://cran.r-project.org/web/packages/Rmpfr/vignettes/log1mexp-note.pdf
+    // with improvements from
+    // https://github.com/JuliaStats/LogExpFunctions.jl/files/8218470/log1pexp.pdf
     use std::f64::consts::LN_2;
     const PRECISION: f64 = f64::MANTISSA_DIGITS as f64;
-    const LN_P_LN2: f64 = 3.6037789929704576; // p_ln2.ln()
+    const LN_OF_P_MUL_LN_2: f64 = 3.6037789929704576; // p_ln2.ln()
     pub const X0: f64 = -PRECISION * LN_2;
     pub const X1: f64 = (PRECISION - 1.0) * LN_2 / 2.0;
     pub const X2: f64 = {
         let p_ln2 = PRECISION * LN_2;
-        p_ln2 + LN_P_LN2 * (1.0 / p_ln2 - 1.0)
+        p_ln2 + LN_OF_P_MUL_LN_2 * (1.0 / p_ln2 - 1.0)
     };
 
     // inverse logit bounds
@@ -21,12 +21,12 @@ pub mod f64 {
 pub mod f32 {
     use std::f32::consts::LN_2;
     const PRECISION: f32 = f32::MANTISSA_DIGITS as f32;
-    const LN_P_LN2: f32 = 2.8115408; // p_ln2.ln()
+    const LN_OF_P_MUL_LN_2: f32 = 2.8115408; // p_ln2.ln()
     pub const X0: f32 = -PRECISION * LN_2;
     pub const X1: f32 = (PRECISION - 1.0) * LN_2 / 2.0;
     pub const X2: f32 = {
         let p_ln2 = PRECISION * LN_2;
-        p_ln2 + LN_P_LN2 * (1.0 / p_ln2 - 1.0)
+        p_ln2 + LN_OF_P_MUL_LN_2 * (1.0 / p_ln2 - 1.0)
     };
 
     pub const ILOGIT_LOWER: f32 = -103.27893; // logit(1.0e-45_f32)
